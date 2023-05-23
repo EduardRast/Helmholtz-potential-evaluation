2f893924ba280c9c4cf900c6854d79ed019fa68d
Eduard <eshkulipa@ucsd.edu>
2023-05-23 01:12:45 -0700

    charge_draft

--- Space.cpp
+++ Space.cpp
@@ -5,31 +5,57 @@
 //  Created by Eduard on 5/1/23.
 //
 
+#include <random>
 #include "Space.hpp"
+
 using namespace Geometry;
 
-struct RandomCoordinateList{
-    RandomCoordinateList(int size){
-        int space[size][3];
+
+Coordinate Space::generate_random_coordinates(){
+static bool check = 0;
+int size = this->size;
+static int** space = new int*[size];
+    if(!check){
+        
+        for (int i = 0; i<size*size*size;i++){
+            space[i] = new int[3];
+        }
+        
+        for (int i = 0; i< size*size*size;){
+            
             for (int z = 0; z<size; z++){
+                
                 for (int y = 0; y<size; y++){
+                    
                     for (int x = 0; x<size; x++){
-                        space[(z+y+x)][0] = x;
-                        space[(z+y+x)][1] = y;
-                        space[(z+y+x)][2] = z;
+                        
+                        space[i][0] = x;
+                        space[i][1] = y;
+                        space[i][2] = z;
+                        //std::cout<<i<<" - "<<space[i][0] << " " << space [i][1] << " " << space[i][2] << "\n";
+                        i++;
                     }
                 }
             }
-    };
-    RandomCoordinateList(const RandomCoordinateList& aRandomCoordinateList){};
-    ~RandomCoordinateList(){};
+            
+        }
+       
+       std::random_device rd;
+       std::mt19937 g(rd());
+       std::shuffle(&space[0][0],&space[size*size*size-1][2],g);
+       check=1;
+    }
     
+    static int count = 0;
     
-};
-/*
-Coordinate Space::generate_random_coordinates(){
+    Coordinate aCoordinate(space[count][0],space[count][1],space[count][2]);
+    
+    return aCoordinate;
+
+    
     
 }
-*/
+
+
 int Space::get_size(){return size;}
 
--- main.cpp
+++ main.cpp
@@ -7,12 +7,6 @@
 
 #include <iostream>
 
-#include <algorithm>
-#include <iterator>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <random>
 
 #include "Space.hpp"
 
@@ -23,37 +17,13 @@
 int main() {
 
     std::cout << "Hello, World!\n";
-    Geometry::Space TheSpace(4);
+    Geometry::Space TheSpace(2);
     std::cout << TheSpace.size <<std::endl;
     
-    int size = 5;
-    int space[size*size*size][3];
-    for (int i = 0; i< std::pow(size,3);){
-        for (int z = 0; z<size; z++){
-            for (int y = 0; y<size; y++){
-                for (int x = 0; x<size; x++){
-                    space[i][0] = x;
-                    space[i][1] = y;
-                    space[i][2] = z;
-                    std::cout<<i<<" - "<<space[i][0] << " " << space [i][1] << " " << space[i][2] << "\n";
-                    i++;
-                }
-            }
-        }
-    }
+    //print random coordinate:
+    Geometry::Coordinate aCoordinate = TheSpace.generate_random_coordinates();
     
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(&space[0][0],&space[size*size*size-1][2],g);
-    
-    
-    // print the result
-    for (int row = 0; row < std::pow(size,3); ++row) {
-        for (int col = 0; col < 3; ++col) {
-            std::cout << space[row][col] << ' ';
-        }
-        std::cout << std::endl;
-    }
+    std::cout<<aCoordinate.x<<" "<<aCoordinate.y<<" "<<aCoordinate.z<<std::endl;
     
     
     return 0;
