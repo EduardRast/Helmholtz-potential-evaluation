713cfbe6419bc8a8e52486b14060b1dfe5893b99
Eduard <eshkulipa@ucsd.edu>
2023-08-02 12:08:35 -0700

    assigning children + generating first level of the tree

--- Box.cpp
+++ Box.cpp
@@ -12,7 +12,7 @@
 std::ostream& operator<<(std::ostream& os, const Box& aBox){
     os<<"Hello, I'm a box"<<std::endl;
     os<<"index: "<<aBox.index<<"\t length: "<<aBox.length<<"\t level: "<<aBox.level<<std::endl;
-    os<<"edge: \n"<<"x: "<<aBox.edge.x<<std::endl<<"y: "<<aBox.edge.y<<std::endl<<"z: "<<aBox.edge.z<<std::endl<<std::endl;
+    os<<"edge: \t"<<"x: "<<aBox.edge.x<<"\t"<<"y: "<<aBox.edge.y<<"\t"<<"z: "<<aBox.edge.z<<std::endl;
     return os;
 }
 
@@ -30,17 +30,16 @@ Tree::Tree(double Dp, int Lp, float Gp){
     this->g = Gp;
     
     //initializing the tree
-    root = new Box;
-    root->level = 0;
-    root->length = D;
-    root->index = 0;
+    root = new_Box_childfree(Geometry::Coordinate(0,0,0), 0, 0);
+    assign_children(root);
+    print_tree();
     std::cout<<"end for now \n";
 }
 
 //assigns children to a box
 bool Tree::assign_children(Box *aBox){
     
-    //find a starting index of the children boxes:
+    //find indexes of the children boxes:
     int parent_level = aBox->level;
     int parent_index = aBox->index;
     int parent_row_length = std::pow(2,parent_level);
@@ -56,12 +55,51 @@ bool Tree::assign_children(Box *aBox){
     
     int child_start_index = child_layer_length*parent_z_in + child_row_length*parent_y_in + 2*parent_x_in;
     
-    int child_indexes[8] = {child_start_index,child_start_index+1,child_start_index+child_row_length,child_start_index+child_row_length+1,child_start_index+child_layer_length,child_start_index+child_layer_length+1};
+    int child_indexes[8] = {
+        child_start_index,
+        child_start_index+1,
+        child_start_index+child_row_length,
+        child_start_index+child_row_length+1,
+        child_start_index+child_layer_length,
+        child_start_index+child_layer_length+1,
+        child_start_index+child_layer_length+child_row_length,
+        child_start_index+child_layer_length+child_row_length+1
+    };
     
-     
+     //find coordinates of the edges of the boxes
+    double parent_x = aBox->edge.x;
+    double parent_y = aBox->edge.y;
+    double parent_z = aBox->edge.z;
+    double child_length = get_length(child_level);
+    
+    Geometry::Coordinate list_of_edges[8] =
+        {Geometry::Coordinate(parent_x,parent_y,parent_z),
+         Geometry::Coordinate(parent_x+child_length,parent_y,parent_z),
+         Geometry::Coordinate(parent_x,parent_y+child_length,parent_z),
+         Geometry::Coordinate(parent_x+child_length,parent_y+child_length,parent_z),
+         Geometry::Coordinate(parent_x,parent_y,parent_z+child_length),
+         Geometry::Coordinate(parent_x+child_length,parent_y,parent_z+child_length),
+         Geometry::Coordinate(parent_x,parent_y+child_length,parent_z+child_length),
+         Geometry::Coordinate(parent_x+child_length,parent_y+child_length,parent_z+child_length)
+        };
+    
+    //assigning the children
+    
+    for (int i = 0; i<8;i++){
+        aBox->children[i] = new_Box_childfree(list_of_edges[i], child_level, child_indexes[i],aBox);
+    }
     
     return 1;
 }
+
+void Tree::print_tree(int levels){
+    std::cout<<"printing the tree \n";
+    std::cout<<*(root);
+    for (int i = 0; i<8; i++){
+        std::cout<<*(root->children[i]);
+    }
+}
+
 
 //creates a new box based on the parameters pased
 Box* Tree::new_Box_childfree(Geometry::Coordinate anEdge, int aLevel ,int anIndex, Box* aParent){
@@ -72,7 +110,10 @@ Box* Tree::new_Box_childfree(Geometry::Coordinate anEdge, int aLevel ,int anInde
     aBox->index = anIndex;
     aBox->length = get_length(aLevel);
     if(aParent!= NULL){aBox->parent = aParent;}
-    
+    for (int i = 0; i<8; i++){
+        aBox->children[i] = NULL;
+    }
+
     return aBox;
 }
 
--- main.cpp
+++ main.cpp
@@ -73,22 +73,16 @@ int main() {
 
 
     std::cout<<"Hello World \n";
-    
+    /*
     Box TheBox;
     Geometry::Coordinate aCoordinate1(1.0,2.2,3.3);
     
     std::cout<<TheBox;
-    
-    Tree theTree(1024, 4);
-    Box* aBox = theTree.new_Box_childfree(aCoordinate1, 1, 3);
-    std::cout<<*aBox;
+    */
+    Tree theTree(512, 4);
+    //Box* aBox = theTree.new_Box_childfree(aCoordinate1, 1, 3);
+    //std::cout<<*aBox;
 
-    //test of the indexes
-    //test();
-    int a = 65;
-    int b = 4;
-    int c = a/b;
-    std::cout<<c<<"\n";
     
     return 0;
 }
